84b6351fce06bc648046a3608c3734190ad67df0
ShelFrench <wsfrench@ucsd.edu>
2022-04-15 09:08:48 -0700

    image fix
    uses if statement to detect "![" and moves index to closed parentheses after.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,11 +11,17 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+
         while(currentIndex < markdown.length()) {
+            if(markdown.charAt(currentIndex) != '!' && markdown.charAt(currentIndex+1) !='['){
+                int nextCParen = markdown.indexOf(")",currentIndex);
+                currentIndex = nextCParen;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             System.out.println(currentIndex);
